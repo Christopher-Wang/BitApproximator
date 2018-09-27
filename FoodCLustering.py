8f8a4f35ef58742d9ebb8205d6ce9d5107a465ba
Christopher Wang <christopher.jj.wang@gmail.com>
2018-09-27 16:18:58 -0400

    Finished some rudimentary data cleaning on the Open Government Nutrition Data 2008

--- FoodCLustering.py
+++ FoodCLustering.py
@@ -1,6 +1,7 @@
 # Example of kNN implemented from Scratch in Python
 import csv
 import pandas as pd
+import numpy as np
 import os
 
 things = []
@@ -23,7 +24,12 @@ for i, data in enumerate(datasets):
 	df = pd.read_csv("Data_Food/" + data, skiprows = headerList[i])
 	dfs.append(df)
 
-print(dfs[2])
+data = pd.concat(dfs, join="inner")
+data = data.set_index('Food name')
+data = data.replace("tr", 0.001)
+data = data.replace(np.nan, 0)
+print(data)
+print(data.loc[data['Protein'] == "tr"])
 # result = set(things[0])
 # for s in things[1:]:
 #     result.intersection_update(s)
